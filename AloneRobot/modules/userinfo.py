e007a2a498c8fc70d040bbda81f6d5b69dfade53
DX CODER <166283380+TheDxCoderteam@users.noreply.github.com>
2024-05-26 03:41:54 +0530

    Version 2.0

--- AloneRobot/modules/userinfo.py
+++ AloneRobot/modules/userinfo.py
@@ -285,7 +285,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' \n[<a href="https://t.me/AloneXBots">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
+        text += ' \n[<a href="https://t.me/THE_CUTE_BOY_OP">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
             bot.username
         )
 
@@ -323,10 +323,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/AloneXBots"
+                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/FRIENDS2FAMILY00"
                             ),
                             InlineKeyboardButton(
-                                "Dɪsᴀsᴛᴇʀ", url="https://t.me/AloneXBots"
+                                "Dɪsᴀsᴛᴇʀ", url="https://t.me/THE_CUTE_BOY_OP"
                             ),
                         ],
                         [
@@ -349,10 +349,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/AloneXBots"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/FRIENDS2FAMILY00"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/AloneXBots"
+                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/THE_CUTE_BOY_OP"
                             ),
                         ],
                         [
